a812394dfb118dbedb32ccb039fbbed12fe18561
abdulfrfr <abdulmails112@gmail.com>
2022-08-01 08:59:31 +0100

    'practicing'

--- calculator.js
+++ calculator.js
@@ -15,6 +15,19 @@ app.post("/", (req, res)=>{
   const result = num1 + num2*num2;
 
   res.send("the result for your calculation is " + result);
+});
+
+app.get("/bmi", (req, res)=>{
+  res.sendFile(__dirname+"/bmi.html");
+});
+
+app.post("/bmi", (req, res)=>{
+  const weight = Number(req.body.weight);
+  const height = Number(req.body.height);
+
+  const result = weight + height * height
+
+  res.send("your bmi result is " + result)
 })
 
 app.listen(4000);
